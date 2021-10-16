70d36124efee2a2b076ff8ae67ef209386bab752
periklis92 <periss92@hotmail.com>
2021-10-16 20:33:10 +0300

    Added comments

--- include/gltxt/gltxt.h
+++ include/gltxt/gltxt.h
@@ -26,13 +26,32 @@
 #include <gltxt/label.h>
 #include <gltxt/font.h>
 #include <vector>
+#include <string>
 
 namespace gltxt
 {
     void Init();
     void Shutdown();
-    unsigned int LoadFontTexture(unsigned char* data, int width, int height);
     void PrepareRender(const float* viewProjMat);
 
-    Label& NewLabelFromFont(const char* txt, const Font& font);
+    Label CreateLabelFromFont(const std::string& txt, const std::string& fontname);
+    
+    /**
+     * @brief Loads a font's data to memory.
+     * 
+     * @param name The name that the font can be found by later.
+     * @param path The path of the file to load the font from.
+     * @param pixelHeight The height of the font to be loaded in pixels.
+     */
+    void LoadFontFromFile(const std::string& name, const std::string& path, int pixelHeight);
+
+    /**
+     * @brief  Loads a font's data to memory.
+     * 
+     * @param name The name that the font can be found by later.
+     * @param data The data of the font file.
+     * @param count The number of bytes of the data.
+     * @param pixelHeight The height of the font to be loaded in pixels.
+     */
+    void LoadFontFromMemory(const std::string& name, const unsigned char* data, size_t count, int pixelHeight);
 }
